d3f11aaf1e890893d836e39247f4272733e076f8
IvanMV <johnmov@mail.ru>
2023-08-08 00:11:02 +0500

    Sets

---
+++ sets/Set .discard(), .remove() & .pop().py
@@ -0,0 +1,16 @@
+n = int(input())
+s = set(map(int, input().split()))
+commands_num = int(input())
+
+for i in range(commands_num):
+    command = input().split()
+    try:
+        if command[0] == 'pop':
+            s.pop()
+        elif command[0] == 'remove':
+            s.remove(int(command[1]))
+        elif command[0] == 'discard':
+            s.discard(int(command[1]))
+    except:
+        None
+print(sum(s))
