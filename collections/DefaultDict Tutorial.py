5f1324e01acc1445403b27bd60381a3ed764caef
IvanMV <johnmov@mail.ru>
2023-08-25 09:55:16 +0500

    Collections

--- collections/DefaultDict Tutorial.py
+++ collections/DefaultDict Tutorial.py
@@ -2,25 +2,25 @@ from collections import defaultdict
 
 n, m = map(int, input().split())
 
-A = []
+first_group = []
 for i in range(n):
-    A.append(input())
+    first_group.append(input())
     
-B = []
+second_group = []
 for i in range(m):
-    B.append(input())
+    second_group.append(input())
     
 my_dict = defaultdict(list)
 
-for key in B:
+for key in second_group:
     if key not in my_dict:
         num = 0
-        for item in A:
+        for item in first_group:
             num += 1
             if key == item:
                 my_dict[key].append(num)
-        if key not in A:
+        if key not in first_group:
             my_dict[key].append(-1)
 
-for key in B:
+for key in second_group:
     print(*my_dict[key])
